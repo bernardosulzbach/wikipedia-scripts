4349e8be3ae9808c3b0ecfd78c773b7e2a28dbc7
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2020-02-16 19:03:26 -0300

    Adds a user-defined number of pages to open
    Also adds timestamps to the logger.

--- open-watchlist-pages.py
+++ open-watchlist-pages.py
@@ -1,13 +1,22 @@
 import json
 import webbrowser
 import logging
-from html.parser import HTMLParser
 import urllib.parse
+import argparse
+from html.parser import HTMLParser
 
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+
+WATCHLIST_XPATH = '//*[@id="mw-content-text"]/div[4]/ul'
 
 LOG_FILENAME = 'open-watchlist-pages.log'
-LOGGING_FORMAT = '%(name)s - %(levelname)s - %(message)s'
+LOG_FORMAT = '[%(asctime)s] %(levelname)s %(module)s.%(funcName)s: %(message)s'
+LOG_DATE_FORMAT = '%Y-%m-%dT%H:%M:%S%z'
+
+COUNT_ARGUMENT_NAME = 'count'
 
 WIKIPEDIA_BASE_URL = 'https://en.wikipedia.org'
 WATCHLIST_URL = WIKIPEDIA_BASE_URL + '/wiki/Special:Watchlist'
@@ -20,8 +29,6 @@ PASSWORD_JSON_FIELD = 'password'
 USERNAME_FIELD_XPATH = '//*[@id="wpName1"]'
 PASSWORD_FIELD_XPATH = '//*[@id="wpPassword1"]'
 LOGIN_BUTTON_XPATH = '//*[@id="wpLoginAttempt"]'
-
-MAXIMUM_OPENED_PAGES = 25
 
 
 def get_expected_field_from_json(json_object, field_name):
@@ -134,7 +141,13 @@ def set_query_parameter(query: str, parameter_name: str, new_parameter_value: st
 
 
 def main():
-    logging.basicConfig(level=logging.INFO, filename=LOG_FILENAME, filemode='a', format=LOGGING_FORMAT)
+    parser = argparse.ArgumentParser(description='Open the most recent unseen pages from your Wikipedia watchlist.')
+    parser.add_argument(COUNT_ARGUMENT_NAME, type=int, help='the number of pages to open')
+    arguments = vars(parser.parse_args())
+    maximum_opened_pages = arguments[COUNT_ARGUMENT_NAME]
+
+    logging.basicConfig(level=logging.INFO, filename=LOG_FILENAME, format=LOG_FORMAT, datefmt=LOG_DATE_FORMAT)
+
     options = webdriver.FirefoxOptions()
     driver = webdriver.Firefox(executable_path='./dependencies/geckodriver', options=options)
 
@@ -144,19 +157,30 @@ def main():
     fill_input_field(driver, USERNAME_FIELD_XPATH, username)
     fill_input_field(driver, PASSWORD_FIELD_XPATH, password)
     driver.find_element_by_xpath(LOGIN_BUTTON_XPATH).click()
+
+    fetched_pages = 0
     opened_pages = 0
-    for watchlist_section in driver.find_elements_by_xpath('//*[@id="mw-content-text"]/div[4]/ul'):
+    unseen_but_not_opened_pages = 0
+
+    wait = WebDriverWait(driver, 10)
+    condition = expected_conditions.presence_of_all_elements_located((By.XPATH, WATCHLIST_XPATH))
+    for watchlist_section in wait.until(condition):
         watchlist_html = watchlist_section.get_attribute('innerHTML')
         parser = WatchlistParser()
         parser.feed(watchlist_html)
         for i, entry in enumerate(parser.watchlist_entries):
             if entry.seen:
                 logging.info('Skipped {} as it was seen.'.format(entry.page_title))
-            elif opened_pages < MAXIMUM_OPENED_PAGES:
+            elif opened_pages < maximum_opened_pages:
                 url = set_query_parameter(WIKIPEDIA_BASE_URL + entry.page_url, 'diff', '0')
                 webbrowser.open(url)
                 opened_pages += 1
                 logging.info('Opened {} ({}).'.format(entry.page_title, url))
+            else:
+                unseen_but_not_opened_pages += 1
+        fetched_pages += len(parser.watchlist_entries)
+    logging.info('Fetched {} page(s).'.format(fetched_pages))
+    logging.info('Unseen entries that were not opened: {}.'.format(unseen_but_not_opened_pages))
     driver.quit()
 
 

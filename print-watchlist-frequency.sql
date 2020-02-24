1b88b4623cebf9b8b5e7628e017a2e5fdd3b3ff2
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2020-02-24 02:10:12 -0300

    Adds a SQL script to print watchlist frequency

---
+++ print-watchlist-frequency.sql
@@ -0,0 +1,9 @@
+SELECT name,
+       count(*)                                                                                AS occurrences,
+       min(date)                                                                               AS first_occurrence,
+       max(date)                                                                               AS last_occurrence,
+       count(*) / ((strftime('%s', max(date)) - strftime('%s', min(date))) / (24.0 * 60 * 60)) AS times_per_day
+FROM page_open
+GROUP BY name
+HAVING count(*) > 1
+ORDER BY occurrences DESC, name;

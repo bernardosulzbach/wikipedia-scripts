c841e706e1a0464d28de5b72cb97a2c65806a25c
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2020-02-24 09:26:13 -0300

    Using current time to evaluate occurrences / day

--- print-watchlist-frequency.sql
+++ print-watchlist-frequency.sql
@@ -3,10 +3,10 @@
 .width 80
 
 SELECT name,
-       count(*)                                                                                AS occurrences,
-       min(date)                                                                               AS first_occurrence,
-       max(date)                                                                               AS last_occurrence,
-       count(*) / ((strftime('%s', max(date)) - strftime('%s', min(date))) / (24.0 * 60 * 60)) AS times_per_day
+       count(*)                                                                                      AS occurrences,
+       min(date)                                                                                     AS first_occurrence,
+       max(date)                                                                                     AS last_occurrence,
+       count(*) / ((strftime('%s', datetime('now')) - strftime('%s', min(date))) / (24.0 * 60 * 60)) AS times_per_day
 FROM page_open
 GROUP BY name
 HAVING count(*) > 1
